ccb7e4eaf246945b5825adb2979bbfbb5ef1d9ac
beapen <github@gulfdoctor.net>
2020-11-26 11:41:30 -0500

    before size

--- ardio/ardio.py
+++ ardio/ardio.py
@@ -1,9 +1,23 @@
 import sys
+import re
 from pdfminer.high_level import extract_pages
 from pdfminer.layout import LTTextContainer, LTChar
 
 def process_args(inputfile: str, outputfile: str):
     print('{0} is the input and {1} is output'.format(inputfile, outputfile))
+    for page_layout in extract_pages(inputfile):
+        for element in page_layout:
+            if isinstance(element, LTTextContainer):
+                # print(element)
+                # print(" : ")
+                print(remove_all_but_alpabets(element.get_text()))
+                print("-------------------------")
+
+
+def remove_all_but_alpabets(input: str):
+    regex = re.compile('[\(\)\[\]0-9]')
+    return regex.sub('', input)
+
 
 if __name__ == "__main__":
     if(len(sys.argv) != 3):
